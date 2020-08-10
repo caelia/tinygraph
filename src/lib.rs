5befcc3f01c3a4613d25548c3320a71271d0be4a
Caelia R Chapin <crc@caelia.net>
2020-08-09 22:01:34 -0600

    It doesn't do anything yet, but it compiles.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,17 @@
 use rusqlite::{params, Connection, Result, Error};
 use std::path::{Path, PathBuf};
 
+pub trait TGStore {
+    type GeneralResult;
+    type ConnectionResult;
+    // type GeneralError;
+    // type ConnectionError;
+    fn new() -> Self;
+    fn setup(&mut self) -> Self::GeneralResult;
+    fn connect(&mut self) -> Self::ConnectionResult;
+    fn disconnect(&mut self) -> Self::ConnectionResult;
+}
+
 
 #[derive(Debug)]
 enum LiteralType {
@@ -28,29 +39,50 @@ struct Edge {
 }
 
 #[derive(Debug)]
-pub struct Database {
+pub struct SqliteTGStore {
     path: PathBuf,
-    conn: Option<Connection>,
+    conn: Option<Box<Connection>>,
 }
 
-impl Database {
+impl TGStore for SqliteTGStore {
+    type GeneralResult = Result<(), Box<dyn std::error::Error>>;
+    type ConnectionResult = Result<(), (Connection, Error)>;
+    // type GeneralError = Box<dyn std::error::Error>;
+    // type ConnectionError = (Error, Connection);
+    fn new() -> Self {
+        SqliteTGStore {
+            path: PathBuf::new(),
+            conn: None
+        }
+    }
+    /*
     pub fn new(path: &PathBuf, overwrite: bool) -> Result<Self, Box<dyn std::error::Error>> {
         if &path.exists() & !overwrite {
             panic!("DB file '{:?}' already exists.", &path);
         }
         let konn = Connection::open(path)?;
-        Ok(Database { path: *path, conn: Some(konn) })
+        Ok(SqliteTGStore { path: *path, conn: Some(konn) })
+    }
+    */
+    fn setup(&mut self) -> Self::GeneralResult {
+        println!("SqliteTGStore::setup()");
+        Ok(())
     }
 
-    pub fn open(&mut self) {
-        if let Ok(konn) = Connection::open(self.path) {
-            self.conn = Some(konn);
+    fn connect(&mut self) -> Self::ConnectionResult {
+        if let Ok(konn) = Connection::open(&self.path) {
+            self.conn = Some(Box::new(konn));
         }
+        Ok(())
     }
 
     // pub fn close(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-    pub fn close(&mut self) -> Result<(), (Connection, Error)> {
-        if let Some(konn) = &self.conn.take() {
+    // pub fn disconnect(&mut self) -> Result<(), (Connection, Error)> {
+    fn disconnect(&mut self) -> Self::ConnectionResult {
+        if let Some(konn) = self.conn.take() {
+            // konn.close()
+            // let _ = konn.close();
+            // ()
             konn.close()
         } else {
             Ok(())

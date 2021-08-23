b9bb5c43a5f5052cff0329eab3a2467c324541f9
Caelia R Chapin <crc@caelia.net>
2021-08-22 18:08:52 -0600

    Simplified ResourceSpec type.

--- src/tg_data.rs
+++ src/tg_data.rs
@@ -1,8 +1,41 @@
-pub enum ResourceType (NODE, EDGE, LITERAL);
+pub enum DataType (
+    INTEGER,
+    FLOAT,
+    BOOLEAN,
+    STRING,
+    DATE,
+    TIME,
+    PERIOD,
+    )
 
+// pub enum ResourceType (NODE, EDGE, LITERAL);
+
+// I *think* the purpose of this type is to identify resources within a single
+// database. So let's simplify it. References to remote resources will be handled
+// separately.
+/*
+pub enum ResourceSpec (
+    INTERNAL_NODE(u32),
+    INTERNAL_EDGE(u32),
+    LOCAL_NODE(String, u32),
+    LOCAL_EDGE(String, u32),
+    REMOTE_NODE(String),
+    REMOTE_EDGE(String),
+    LITERAL(DataType, String),
+    )
+*/
+
+pub enum ResourceSpec (
+    NODE(u32),
+    EDGE(u32),
+    LITERAL(DataType, String),
+)
+
+/*
 struct ResourceSpec {
     restype: ResourceType,
     id: u32,
 }
+*/
 
 struct TGData {};

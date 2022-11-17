b10161c20358cb896a06c30d61f90e5754a8cf2f
Caelia R Chapin <crc@caelia.net>
2022-11-16 23:25:26 -0700

    Adding some actual code to create DBs. It's very great!

--- src/bin/tgm.rs
+++ src/bin/tgm.rs
@@ -1,6 +1,14 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
 use std::path::PathBuf;
 use structopt::StructOpt;
+use tinygraph::Database;
+use tinygraph::cli::Tgm;
 
 fn main() {
-    println!("Hullow!");
+    let tgm = Tgm::from_args();
+    let path, cmd = match tgm {
+        Tgm { path. cmd }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,27 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
+#![allow(unused_imports)]
+
+pub mod cli;
 
 use rusqlite::{params, Connection, Result};
 use std::path::{Path, PathBuf};
+use std::error::Error;
+use std::fmt;
 // use std::rc::Rc;
+
+#[derive(Debug)]
+struct TinyGraphError {
+    desc: String,
+}
+
+impl fmt::Display for TinyGraphError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "TinyGraph Error!!!!!!!!!!!!!!!!")
+    }
+}
+
+impl Error for TinyGraphError {}
 
 
 #[derive(Debug)]
@@ -30,24 +48,56 @@ struct Edge {
 }
 
 #[derive(Debug)]
+enum DbOptions {
+    Read,
+    Write,
+}
+
+#[derive(Debug)]
 pub struct Database<'a> {
     path: &'a Path,
     conn: Option<Connection>,
+    options: Vec<DbOptions>,
 }
 
 impl<'a> Database<'a> {
-    pub fn new(path: &'a Path, overwrite: bool) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn new(path: &'a Path, name: Option<String>, init: bool,
+            overwrite: bool, create_path: bool, options: Vec<DbOptions>)
+            -> Result<Self, Box<dyn std::error::Error>> {
+        let filename = match name {
+            Some(fname) => Path::new(&fname),
+            None => Path::new("tgr_data.db"),
+        };
+        let full_path = path.join(filename);
+        let ok = if init {
+            match Self::initialize(full_path, overwrite).unwrap() {
+                Ok(_) => true,
+                Err(_) => false,
+            }
+        } else {
+            false
+        };
+        if ok {
+            Ok(Database { path: &path, conn: None, options })
+        } else {
+            Err(Box::new(TinyGraphError { desc: String::from("Unable to create database!")}))
+        }
+    }
+    
+    fn initialize(path: &'a Path, overwrite: bool) -> Result<(), Box<dyn std::error::Error>> {
         if &path.exists() & !overwrite {
             panic!("DB file '{:?}' already exists.", &path);
         }
-        let konn = Connection::open(&path)?;
-        Ok(Database { path: &path, conn: Some(konn) })
+        Ok(())
     }
 
     pub fn open(&mut self) {
         if let Ok(konn) = Connection::open(self.path) {
             self.conn = Some(konn);
         }
+    }
+    
+    pub fn connect(&mut self) {
     }
 
     // Should return a Result

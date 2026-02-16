f042c29d2764a107700c2bad8b2ac9e7daabd333
Caelia R Chapin <crc@caelia.net>
2026-02-16 05:44:33 -0700

    Added 'static lifetime to constants in sql.rs.

--- src/sqlite/sql.rs
+++ src/sqlite/sql.rs
@@ -1,4 +1,4 @@
-pub const CREATE_REL_TABLE: &str = "
+pub const CREATE_REL_TABLE: &'static str = "
     CREATE TABLE relations (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         relname TEXT NOT NULL,
@@ -7,14 +7,14 @@ pub const CREATE_REL_TABLE: &str = "
     );
 ";
 
-pub const CREATE_TYPE_TABLE: &str = "
+pub const CREATE_TYPE_TABLE: &'static str = "
     CREATE TABLE types (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         typename TEXT NOT NULL
     );
 ";
 
-pub const CREATE_DATA_TABLE: &str = "
+pub const CREATE_DATA_TABLE: &'static str = "
     CREATE TABLE data (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         node_id INTEGER NOT NULL,
@@ -24,4 +24,4 @@ pub const CREATE_DATA_TABLE: &str = "
     );
 ";
 
-pub const POPULATE_TYPE_TABLE: &str = "INSERT INTO types (typename) VALUES (?);";
+pub const POPULATE_TYPE_TABLE: &'static str = "INSERT INTO types (typename) VALUES (?);";
